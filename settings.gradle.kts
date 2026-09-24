da36f9d2ea525547cdbe26e4b2ff4b0f7162c759
rohithh090 <mrohith090@gmail.com>
2026-09-24 19:20:36 +0530

    Fix single quote syntax errors in gradle kts files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,16 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = 'FocusFeed'
-include(':app')
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "FocusFeed"
+include(":app")
